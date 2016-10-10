2c7293d14cf9af6ec2c05b8da02e030ba3c1d8d7
Richard Kwan <taekyung.kwan@gmail.com>
2016-10-10 11:01:47 -0400

    add search functionality to homepage search

--- frontend/components/home_container/home.jsx
+++ frontend/components/home_container/home.jsx
@@ -1,23 +1,67 @@
 import React from 'react';
+import { hashHistory } from 'react-router';
 
-const Home = () => {
-  return (
-    <main className="home-page">
-      <heading className="heading-box">
-        <h1 className="main-heading">Live There</h1>
-        <h3 className="heading-sentence">
-          Book homes from local hosts and experience a place like you live there
-        </h3>
-      </heading>
-      <div className="home-search-listings">
-        <h2 className="search-heading">
-          Search over 2 million homes in over 190 countries
-        </h2>
-        <input type="text" placeholder="spot search here" className="temp-search" />
-      </div>
-    </main>
+class Home extends React.Component {
 
-  );
-};
+  constructor(props) {
+    super(props);
+    this.searchSubmit = this.searchSubmit.bind(this);
+
+  }
+
+  componentDidMount() {
+    const defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(-16.560867, -151.804396),
+      new google.maps.LatLng(-16.434973, -151.678022)
+    );
+
+    const homeSearch = document.getElementById("homepage-search");
+    const options = {
+      bounds: defaultBounds
+    };
+
+    const autocomplete = new google.maps.places.Autocomplete(homeSearch, options);
+    this.autocomplete = autocomplete;
+  }
+
+
+
+  searchSubmit(e) {
+    e.preventDefault();
+    const searchLocation = this.autocomplete.getPlace().geometry.location;
+    const mapCenter = { lat: searchLocation.lat(),
+                        lng: searchLocation.lng() };
+    hashHistory.push({
+      pathname: '/search',
+      query: mapCenter
+    });
+  }
+
+  render() {
+    return (
+      <main className="home-page">
+        <heading className="heading-box">
+          <h1 className="main-heading">Live There</h1>
+          <h3 className="heading-sentence">
+            Book homes from local hosts and experience a place like you live there
+          </h3>
+        </heading>
+        <div className="home-search-listings">
+          <h2 className="search-heading">
+            Search over 2 million homes in over 190 countries
+          </h2>
+          <form onSubmit={this.searchSubmit} >
+            <input
+              type="text"
+              placeholder="Where to?"
+              className="home-search"
+              id="homepage-search"
+              />
+          </form>
+        </div>
+      </main>
+    );
+  }
+}
 
 export default Home;
--- frontend/components/spot_map/spot_map.jsx
+++ frontend/components/spot_map/spot_map.jsx
@@ -3,13 +3,28 @@ import MarkerManager from '../../util/marker_manager';
 
 class SpotMap extends React.Component {
 
+  getQueryParameters() {
+    return document.location.hash.slice(9)
+      .replace(/(^\?)/,'').split("&")
+      .map(function(n) {
+        n = n.split("=");
+        this[n[0]] = n[1];
+        return this;
+      }
+      .bind({}))[0];
+  }
+
   componentDidMount() {
     const mapDOMNode = this.refs.map;
     const mapOptions = {
       center: { lat: -16.498855, lng: -151.741460 },
       zoom: 13
     };
-
+    const queryOptions = this.getQueryParameters();
+    const queryCenter = { center:
+      { lat: parseFloat(queryOptions.lat), lng: parseFloat(queryOptions.lng) }
+    };
+    Object.assign(mapOptions, queryCenter);
     this.map = new google.maps.Map(mapDOMNode, mapOptions);
     this.MarkerManager = new MarkerManager(this.map);
     this._registerListeners();
