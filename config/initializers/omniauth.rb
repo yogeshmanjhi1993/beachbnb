6fd363a1e6487e11688034921f9b8a60c3f24e6a
Richard Kwan <taekyung.kwan@gmail.com>
2016-11-03 15:02:56 -0400

    Add keys to figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.full_host = Rails.env.production? ? 'http://www.beachbnb.co' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '395271017304-dnog3n8u9s5knshcapsba6tn52dr4efn.apps.googleusercontent.com', 'jxzp_1UayVr_h5m_XUP2MEjP', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['oauthid'], ENV['oauthsecret'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
